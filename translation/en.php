48ed31b82dff9cbf2994f43a6dbff667b71b8c6e
DGideas <dgideaswikipedia@gmail.com>
2013-04-12 23:22:24 +0800

    Create en.php

---
+++ translation/en.php
@@ -0,0 +1,15 @@
+<?php
+    //Please update other translation, even it is null
+    $ideastext=array();
+    $ideastext["ad"]="advertisement";
+    $ideastext["bot"]="bot";
+    $ideastext["done"]="done";
+    $ideastext["error"]="error";
+    $ideastext["finish"]="finish";
+    $ideastext["no"]="no";
+    $ideastext["ok"]="ok";
+    $ideastext["report"]="report";
+    $ideastext["user"]="user";
+    $ideastext["warning"]="warning";
+    $ideastext["yes"]="yes";
+?>
