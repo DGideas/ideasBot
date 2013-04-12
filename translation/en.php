c5bd65cef2ef244cc893f4d8ff366df8353ca143
DGideas <dgideaswikipedia@gmail.com>
2013-04-13 07:08:00 +0800

    Update en.php

--- translation/en.php
+++ translation/en.php
@@ -1,15 +1,15 @@
 <?php
     //Please update other translation, even it is null
     $ideastext=array();
-    $ideastext["ad"]="advertisement";
-    $ideastext["bot"]="bot";
-    $ideastext["done"]="done";
-    $ideastext["error"]="error";
-    $ideastext["finish"]="finish";
-    $ideastext["no"]="no";
-    $ideastext["ok"]="ok";
-    $ideastext["report"]="report";
-    $ideastext["user"]="user";
-    $ideastext["warning"]="warning";
-    $ideastext["yes"]="yes";
+    $ideastext["en"]["ad"]="advertisement";
+    $ideastext["en"]["bot"]="bot";
+    $ideastext["en"]["done"]="done";
+    $ideastext["en"]["error"]="error";
+    $ideastext["en"]["finish"]="finish";
+    $ideastext["en"]["no"]="no";
+    $ideastext["en"]["ok"]="ok";
+    $ideastext["en"]["report"]="report";
+    $ideastext["en"]["user"]="user";
+    $ideastext["en"]["warning"]="warning";
+    $ideastext["en"]["yes"]="yes";
 ?>
