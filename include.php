3cb50e6d5169db1da758985fc759b2a23b12e7a7
DGideas <dgideaswikipedia@gmail.com>
2013-04-13 07:24:09 +0800

    Update include.php

--- include.php
+++ include.php
@@ -2,5 +2,6 @@
     include_once ('config.php');
     include_once ('translation.php');
     include_once ('ideasfunc1.php');
+    include_once ('ideasfunc2.php');
     include_once ('zhwpfunc1.php');
 ?>
