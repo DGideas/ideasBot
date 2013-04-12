697975281ded68a03fbddbf3426689a196b0e78b
DGideas <dgideaswikipedia@gmail.com>
2013-04-12 23:36:55 +0800

    Update example.php

--- example.php
+++ example.php
@@ -1,8 +1,6 @@
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <?php
-include_once ('config.php');
-include_once ('zhwpfunc1.php');
-include_once ('ideasfunc1.php');
+include_once ('include.php');
 ideas_login();
 
 zhwp_check_50();
@@ -10,7 +8,5 @@ zhwp_check_ad();
 zhwp_clean_sandbox();
 zhwp_clean_pic_sandbox();
 
-//最后几行
-echop ();
-echo "已登出";
+
 ?>
