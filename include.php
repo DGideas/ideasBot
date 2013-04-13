6a4824033095c9eab17f73c1239eb4071515fea7
DGideas <dgideaswikipedia@gmail.com>
2013-04-13 14:39:29 +0800

    Update include.php

--- include.php
+++ include.php
@@ -3,5 +3,6 @@
     include_once ('translation.php');
     include_once ('ideasfunc1.php');
     include_once ('ideasfunc2.php');
-    include_once ('zhwpfunc1.php');
+    //将您使用的拓展置于此处
+    include_once ('extension/example.php'); //这是一个例子
 ?>
