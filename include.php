04d635733db2e362a0ad51110c3cc88ddbbdcade
DGideas <dgideaswikipedia@gmail.com>
2013-04-13 14:53:36 +0800

    Update include.php

--- include.php
+++ include.php
@@ -4,5 +4,5 @@
     include_once ('ideasfunc1.php');
     include_once ('ideasfunc2.php');
     //将您使用的拓展置于此处
-    include_once ('extension/example.php'); //这是一个例子
+    include_once ('extension/example.php'); //这是一个例子,您可以覆盖它或者仿照它添加拓展
 ?>
