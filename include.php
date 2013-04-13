d5f6340968e0f1596d4ecc3f11401add851774e6
DGideas <dgideaswikipedia@gmail.com>
2013-04-14 07:26:17 +0800

    debug

--- include.php
+++ include.php
@@ -1,8 +1,8 @@
 <?php
-    include_once ('config.php');
-    include_once ('translation.php');
-    include_once ('ideasfunc1.php');
-    include_once ('ideasfunc2.php');
+    include_once (dirname(__FILE__)."/config.php");
+    include_once (dirname(__FILE__)."/translation.php");
+    include_once (dirname(__FILE__)."/ideasfunc1.php");
+    include_once (dirname(__FILE__)."/ideasfunc2.php");
     //将您使用的拓展置于此处
-    include_once ('extension/example.php'); //这是一个例子,您可以覆盖它或者仿照它添加拓展
+    include_once (dirname(__FILE__)."/extension/example.php"); //这是一个例子,您可以覆盖它或者仿照它添加拓展
 ?>
