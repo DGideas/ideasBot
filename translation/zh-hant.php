f9da09a0b540ceb0017a33069f176a2335307726
DGideas <dgideaswikipedia@gmail.com>
2013-04-13 15:55:36 +0800

    Update zh-hant.php

--- translation/zh-hant.php
+++ translation/zh-hant.php
@@ -1,18 +1,17 @@
 <?php
     //請務必同時更新其他語言版本的翻譯,即使留空
-    $ideastext=array();
-    $ideastext["zh-hant"]["ad"]="广告";
-    $ideastext["zh-hant"]["bot"]="机器人";
-    $ideastext["zh-hant"]["done"]="完成";
-    $ideastext["zh-hant"]["error"]="错误";
-    $ideastext["zh-hant"]["finish"]="完成";
-    $ideastext["zh-hant"]["loginfailed"]="登录失败";
-    $ideastext["zh-hant"]["loginsuccess"]="登录成功";
-    $ideastext["zh-hant"]["needRW"]="请检查cookie临时文件是否具有RWRWRW权限";
-    $ideastext["zh-hant"]["no"]="不";
-    $ideastext["zh-hant"]["ok"]="完成";
-    $ideastext["zh-hant"]["report"]="报告";
-    $ideastext["zh-hant"]["user"]="用户";
-    $ideastext["zh-hant"]["warning"]="警告";
-    $ideastext["zh-hant"]["yes"]="是";
+    $GLOBALS["ideastext"]["zh-hant"]["ad"]="广告";
+    $GLOBALS["ideastext"]["zh-hant"]["bot"]="机器人";
+    $GLOBALS["ideastext"]["zh-hant"]["done"]="完成";
+    $GLOBALS["ideastext"]["zh-hant"]["error"]="错误";
+    $GLOBALS["ideastext"]["zh-hant"]["finish"]="完成";
+    $GLOBALS["ideastext"]["zh-hant"]["loginfailed"]="登录失败";
+    $GLOBALS["ideastext"]["zh-hant"]["loginsuccess"]="登录成功";
+    $GLOBALS["ideastext"]["zh-hant"]["needRW"]="请检查cookie临时文件是否具有RWRWRW权限";
+    $GLOBALS["ideastext"]["zh-hant"]["no"]="不";
+    $GLOBALS["ideastext"]["zh-hant"]["ok"]="完成";
+    $GLOBALS["ideastext"]["zh-hant"]["report"]="报告";
+    $GLOBALS["ideastext"]["zh-hant"]["user"]="用户";
+    $GLOBALS["ideastext"]["zh-hant"]["warning"]="警告";
+    $GLOBALS["ideastext"]["zh-hant"]["yes"]="是";
 ?>
