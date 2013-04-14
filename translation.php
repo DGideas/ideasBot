e6c0edae74af2d2a288d60d1861b7dec46dca429
DGideas <dgideaswikipedia@gmail.com>
2013-04-14 15:44:31 +0800

    debug

--- translation.php
+++ translation.php
@@ -1,4 +1,5 @@
 <?php
+    $ideastext=array();
     include_once (dirname(__FILE__)."/translation/en.php");
     include_once (dirname(__FILE__)."/translation/zh-hans.php");
     include_once (dirname(__FILE__)."/translation/zh-hant.php");
