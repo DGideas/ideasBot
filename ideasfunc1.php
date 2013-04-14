8005a5e24e011b02711cfc7ecd807d714d08ba7e
DGideas <dgideaswikipedia@gmail.com>
2013-04-14 12:23:25 +0800

    Update ideasfunc1.php

--- ideasfunc1.php
+++ ideasfunc1.php
@@ -245,7 +245,7 @@ function ideas_get_user_contribs($user,$times="100"){
 //$title:指明了目标页面,这个参数是必需的
 //$intoken:指明了需要获得的令牌种类,默认为edit
 //API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+info
-function ideasgettoken($title,$intoken="edit"){
+function ideas_get_token($title,$intoken="edit"){
     $post="action=query&prop=info&titles=".$title."&intoken=".$intoken;
     $data=ideas_connect($post);
     //分析数据
@@ -253,37 +253,13 @@ function ideasgettoken($title,$intoken="edit"){
     return $xml->query->pages->page[0]->attributes()->edittoken;
 }
 
-//该函数用于编辑条目顶部(添加管理模版等)(标题,内容,摘要)
-//WARNING:函数不稳定
-//$title:指明了目标页面的标题,这个参数是必需的
-//$text:指明了第0段的内容
-//$summary:指明了编辑摘要,默认为空
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=edit
-function ideasedittop($title,$text,$summary=""){
-    //步骤1:获得edittoken
-    $edittoken=ideasgettoken($title,"edit");
-    $edittokenhtml=str_ireplace("+\\","%2B%5C",$edittoken); //自动将末尾+\ HTML编码为%2B%5C
-    $text=urlencode($text);//HTML编码
-    $summary=urlencode($summary);//HTML编码
-    //步骤2:添加新段落
-    if ($summary==""){
-        $post="action=edit&title=".$title."&section=0&text=".$text."&token=".$edittokenhtml;
-    }else{
-        $summary=ideas_summary($summary); //处理编辑摘要头尾
-        $post="action=edit&title=".$title."&section=0&text=".$text."&token=".$edittokenhtml."&summary=".$summary;
-    }
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    return $xml;
-}
 //该函数用于编辑条目(标题,内容,摘要)(覆盖!)
 //WARNING:本函数会覆盖!页面原有内容
 //$title:指明了目标页面的标题,这个参数是必需的
 //$text:指明了页面的内容
 //$summary:指明了编辑摘要,默认为空
 //API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=edit
-function ideasedit($title,$text,$summary=""){
+function ideas_edit($title,$text,$summary=""){
     //步骤1:获得edittoken
     $edittoken=ideasgettoken($title,"edit");
     $edittokenhtml=str_ireplace("+\\","%2B%5C",$edittoken); //自动将末尾+\ HTML编码为%2B%5C
@@ -302,6 +278,30 @@ function ideasedit($title,$text,$summary=""){
     return $xml;
 }
 
+//该函数用于编辑条目顶部(添加管理模版等)(标题,内容,摘要)
+//WARNING:函数不稳定
+//$title:指明了目标页面的标题,这个参数是必需的
+//$text:指明了第0段的内容
+//$summary:指明了编辑摘要,默认为空
+//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=edit
+function ideas_edit_top($title,$text,$summary=""){
+    //步骤1:获得edittoken
+    $edittoken=ideasgettoken($title,"edit");
+    $edittokenhtml=str_ireplace("+\\","%2B%5C",$edittoken); //自动将末尾+\ HTML编码为%2B%5C
+    $text=urlencode($text);//HTML编码
+    $summary=urlencode($summary);//HTML编码
+    //步骤2:添加新段落
+    if ($summary==""){
+        $post="action=edit&title=".$title."&section=0&text=".$text."&token=".$edittokenhtml;
+    }else{
+        $summary=ideas_summary($summary); //处理编辑摘要头尾
+        $post="action=edit&title=".$title."&section=0&text=".$text."&token=".$edittokenhtml."&summary=".$summary;
+    }
+    $data=ideas_connect($post);
+    //分析数据
+    $xml = simplexml_load_string($data);
+    return $xml;
+}
 
 //该函数用于添加新段落(标题,段落标题,内容,编辑摘要)
 //WARNING:函数不稳定
@@ -310,7 +310,7 @@ function ideasedit($title,$text,$summary=""){
 //$text:指明了页面的内容
 //$summary:指明了编辑摘要,默认为空
 //API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=edit
-function ideaseditnew($title,$sectiontitle,$text,$summary=""){
+function ideas_edit_new($title,$sectiontitle,$text,$summary=""){
     //步骤1:获得edittoken
     $edittoken=ideasgettoken($title,"edit");
     $edittokenhtml=str_ireplace("+\\","%2B%5C",$edittoken); //自动将末尾+\ HTML编码为%2B%5C
