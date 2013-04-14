8609b0df0562c39913b79082506b802aad0a0d70
DGideas <dgideaswikipedia@gmail.com>
2013-04-14 12:24:30 +0800

    see 8005a5e24e011b02711cfc7ecd807d714d08ba7e

--- extension/tfsfunc1.php
+++ extension/tfsfunc1.php
@@ -5,17 +5,17 @@
 //目前只支持大小为46字节的沙箱
 function tfs_clean_sandbox($sandboxname="Sandbox"){
     if (ideas_get_size($sandboxname)!="46"){
-        $author=ideasgetauthor($sandboxname);
+        $author=ideas_get_author($sandboxname);
         $user=$author->query->pages->page->revisions->rev[0]->attributes()->user;
-        ideasedit($sandboxname,"{{请在本段文字下方尝试您的编辑}}","清理沙盒");
-        ideaslog("清理了沙盒:[[".$sandboxname."]],最近编者为:".$user);
+        ideas_edit($sandboxname,"{{请在本段文字下方尝试您的编辑}}","清理沙盒");
+        ideas_log("清理了沙盒:[[".$sandboxname."]],最近编者为:".$user);
     }else{
             if (ideasstrfind(ideas_view($sandboxname),"{{请在本段文字下方尝试您的编辑}}")==true){
         }else{
-            $author=ideasgetauthor($sandboxname);
+            $author=ideas_get_author($sandboxname);
             $user=$author->query->pages->page->revisions->rev[0]->attributes()->user;
-            ideasedit($sandboxname,"{{请在本段文字下方尝试您的编辑}}","清理沙盒");
-            ideaslog("清理了沙盒:[[".$sandboxname."]],最近编者为:".$user);
+            ideas_edit($sandboxname,"{{请在本段文字下方尝试您的编辑}}","清理沙盒");
+            ideas_log("清理了沙盒:[[".$sandboxname."]],最近编者为:".$user);
         }
     }
     return;
