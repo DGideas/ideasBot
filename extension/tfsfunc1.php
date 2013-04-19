2d3a68e7808bf539ad9baa5e5fb65bd1d88de654
DGideas <dgideaswikipedia@gmail.com>
2013-04-19 19:53:19 +0800

    see cf5946712dc92cbfb57ee5f0fb36e94ad9d7e764

--- extension/tfsfunc1.php
+++ extension/tfsfunc1.php
@@ -10,7 +10,7 @@ function tfs_clean_sandbox($sandboxname="Sandbox"){
         ideas_edit($sandboxname,"{{请在本段文字下方尝试您的编辑}}","清理沙盒");
         ideas_log("清理了沙盒:[[".$sandboxname."]],最近编者为:".$user);
     }else{
-            if (ideasstrfind(ideas_view($sandboxname),"{{请在本段文字下方尝试您的编辑}}")==true){
+            if (ideas_str_find(ideas_view($sandboxname),"{{请在本段文字下方尝试您的编辑}}")==true){
         }else{
             $author=ideas_get_author($sandboxname);
             $user=$author->query->pages->page->revisions->rev[0]->attributes()->user;
