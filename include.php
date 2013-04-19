8bd571aad4077578a05d5fa93696fc97e564003f
ideasLab <wangwanlin2468@foxmail.com>
2013-04-19 18:25:32 +0800

    add preinclude extensions

--- include.php
+++ include.php
@@ -4,5 +4,8 @@
     include_once (dirname(__FILE__)."/ideasfunc1.php");
     include_once (dirname(__FILE__)."/ideasfunc2.php");
     //将您使用的拓展置于此处
+    //include_once (dirname(__FILE__)."/extension/tfsfunc1.php"); //取消注释以启用该拓展
+    //include_once (dirname(__FILE__)."/extension/zhwpfunc1.php"); //取消注释以启用该拓展
+    //include_once (dirname(__FILE__)."/extension/zhmgfunc1.php"); //取消注释以启用该拓展
     include_once (dirname(__FILE__)."/extension/example.php"); //这是一个例子,您可以覆盖它或者仿照它添加拓展
 ?>
