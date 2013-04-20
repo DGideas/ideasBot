9563a3623811a5ee5bc7627d487fa4a75c0e3a4a
DGideas <dgideaswikipedia@gmail.com>
2013-04-20 12:40:01 +0800

    Update ideasdb.php

--- extension/ideasdb.php
+++ extension/ideasdb.php
@@ -1,4 +1,14 @@
 <?php
 //encode:UTF-8
+$ideasdb=array();
 
+//该函数用于读取数据库设置
+function ext_get_ideasdb_conf(){
+    $get=file_get_contents("db/ideasdb.conf");
+    preg_match("/dbname:([0-9a-zA-Z]*)\sdbusername:([0-9a-zA-Z]*)\sdbpassword:([0-9a-zA-Z]*)/",$get,$reg);
+    $GLOBALS["ideasdb"]["dbname"]=$reg[1];
+    $GLOBALS["ideasdb"]["dbusername"]=$reg[2];
+    $GLOBALS["ideasdb"]["dbpassword"]=$reg[3];
+    return;
+}
 ?>
