4fe60a29138f4e08ff31176c36841d5be4c2ad1e
DGideas <dgideaswikipedia@gmail.com>
2013-04-20 10:58:09 +0800

    Update include.php

--- include.php
+++ include.php
@@ -4,6 +4,7 @@
     include_once (dirname(__FILE__)."/ideasfunc1.php");
     include_once (dirname(__FILE__)."/ideasfunc2.php");
     //将您使用的拓展置于此处
+    //include_once (dirname(__FILE__)."/extension/ideasdb.php"); //取消注释以启用该拓展
     //include_once (dirname(__FILE__)."/extension/tfsfunc1.php"); //取消注释以启用该拓展
     //include_once (dirname(__FILE__)."/extension/zhwpfunc1.php"); //取消注释以启用该拓展
     //include_once (dirname(__FILE__)."/extension/zhmgfunc1.php"); //取消注释以启用该拓展
