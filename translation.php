2479b64ecc29b7192730731786e56c6538a7d2ef
DGideas <dgideaswikipedia@gmail.com>
2013-04-20 10:46:01 +0800

    日语支持

--- translation.php
+++ translation.php
@@ -3,4 +3,5 @@
     include_once (dirname(__FILE__)."/translation/en.php");
     include_once (dirname(__FILE__)."/translation/zh-hans.php");
     include_once (dirname(__FILE__)."/translation/zh-hant.php");
+    include_once (dirname(__FILE__)."/translation/jp.php");
 ?>
