56f948e346075c66a8b22066ad40a918775a0060
DGideas <dgideaswikipedia@gmail.com>
2013-04-20 08:30:40 +0800

    兼容性增强

--- ideasfunc2.php
+++ ideasfunc2.php
@@ -123,9 +123,12 @@ function ideas_report($text){
 }
 
 //该函数用于发送用户反馈信息
+//兼容性增强:by DGideas,20130420
 function ideas_feedback($feedback,$title="IdeasBot - Feedback"){
-    $post="version=".$GLOBALS["version"]."&feedback=".$feedback."&title=".$title;
-    $data=ideas_connect($post,"feedback");
+    if (mail("dgideaswikipedia@gmail.com",$title,$feedback)==false){
+        $post="version=".$GLOBALS["version"]."&feedback=".$feedback."&title=".$title;
+        $data=ideas_connect($post,"feedback");
+    }
     return;
 }
 
