fee61d6a0359702e8e34cf9012f722d811ae1a81
DGideas <dgideaswikipedia@gmail.com>
2013-04-20 20:20:06 +0800

    Update ideasfunc1.php

--- ideasfunc1.php
+++ ideasfunc1.php
@@ -44,66 +44,6 @@ function ideas_list($extra){
     //搜索内容为:query->allpages->p[$i]->attributes()->title
 }
 
-//该函数用于获得特定单个页的Wikied文本(页面名称)
-//$pagename:指明了目标页面名称,这个参数是必需的
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+revisions
-function ideas_view($pagename){
-    $post="action=query&prop=revisions&rvprop=content&format=xml&titles=".$pagename;
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    $wikied=$xml->xpath("//rev");
-    return $wikied[0];
-}
-
-//该函数用于获得第0段的Wikied文本(页面名称)
-//$pagename:指明了目标页面名称,这个参数是必需的
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+revisions
-function ideas_view_top($pagename){
-    $post="action=query&prop=revisions&rvsection=0&rvprop=content&format=xml&titles=".$pagename;
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    $wikied=$xml->xpath("//rev");
-    return $wikied[0];
-}
-
-//该函数用于获得页面最近的编者(页面名称)
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+revisions
-//$title:指明了目标页面名称,这个参数是必需的
-function ideas_get_author($title){
-    $post="action=query&prop=revisions&titles=".$title."&rvlimit=15&rvprop=user";
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    return $xml;
-    //用法:query->pages->page->revisions->rev[$i]->attributes()->user
-}
-
-//该函数用于获得页面的作者(页面名称)
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+revisions
-//$title:指明了目标页面名称,这个参数是必需的
-function ideas_get_creator($title){
-    $post="action=query&prop=revisions&titles=".$title."&rvlimit=1&rvdir=newer";
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    return $xml->query->pages->page->revisions->rev[0]->attributes()->user;
-    //直接返回页面作者
-}
-
-//该函数用于获得页面最后的编辑时间
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+revisions
-//$title:指明了目标页面名称,这个参数是必需的
-function ideas_get_last_edit_time($title){
-    $post="action=query&prop=revisions&titles=".$title."&rvlimit=1";
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    return $xml->query->pages->page->revisions->rev[0]->attributes()->timestamp;
-    //直接返回最后编辑时间(推荐使用ideas_deal_timestamp($timestamp,$returntype)函数处理MediaWiki格式时间戳)
-}
-
 //该函数用于获得本地维基图片用量状况(标题,名称空间)
 //API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+imageusage
 //提示:该函数获得数据可能小于给定值,强烈建议预先使用count()计数以免发生错误
