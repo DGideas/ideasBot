b43d582498fddd9a7b84228a74285f8f291568d3
DGideas <dgideaswikipedia@gmail.com>
2013-04-20 11:18:56 +0800

    兼容性

--- include.php
+++ include.php
@@ -1,12 +1,12 @@
 <?php
     include_once (dirname(__FILE__)."/config.php");
-    include_once (dirname(__FILE__)."/translation.php");
-    include_once (dirname(__FILE__)."/ideasfunc1.php");
-    include_once (dirname(__FILE__)."/ideasfunc2.php");
+    include_once ("translation.php");
+    include_once ("ideasfunc1.php");
+    include_once ("ideasfunc2.php");
     //将您使用的拓展置于此处
-    //include_once (dirname(__FILE__)."/extension/ideasdb.php"); //取消注释以启用该拓展
-    //include_once (dirname(__FILE__)."/extension/tfsfunc1.php"); //取消注释以启用该拓展
-    //include_once (dirname(__FILE__)."/extension/zhwpfunc1.php"); //取消注释以启用该拓展
-    //include_once (dirname(__FILE__)."/extension/zhmgfunc1.php"); //取消注释以启用该拓展
-    include_once (dirname(__FILE__)."/extension/example.php"); //这是一个例子,您可以覆盖它或者仿照它添加拓展
+    //include_once ("extension/ideasdb.php"); //取消注释以启用该拓展
+    //include_once ("extension/tfsfunc1.php"); //取消注释以启用该拓展
+    //include_once ("extension/zhwpfunc1.php"); //取消注释以启用该拓展
+    //include_once ("extension/zhmgfunc1.php"); //取消注释以启用该拓展
+    include_once ("extension/example.php"); //这是一个例子,您可以覆盖它或者仿照它添加拓展
 ?>
