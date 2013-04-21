4c5aef4b6085d88f8363d0775c85fd856a31cf1e
DGideas <dgideaswikipedia@gmail.com>
2013-04-21 08:13:18 +0800

    Update ideasfunc1.php

--- ideasfunc1.php
+++ ideasfunc1.php
@@ -74,16 +74,4 @@ function ideas_get_user_contribs($user,$times="100"){
     //名称空间:query->usercontribs->item[$i]->attributes()->ns
 }
 
-//该函数用于获取执行相应动作的token(标题,动作)
-//$title:指明了目标页面,这个参数是必需的
-//$intoken:指明了需要获得的令牌种类,默认为edit
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+info
-function ideas_get_token($title,$intoken="edit"){
-    $post="action=query&prop=info&titles=".$title."&intoken=".$intoken;
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    return $xml->query->pages->page[0]->attributes()->edittoken;
-}
-
 ?>
