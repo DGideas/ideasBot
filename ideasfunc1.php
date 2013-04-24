8539a5f7c94cf1e21b85f5ace95380efcf495ecc
DGideas <dgideaswikipedia@gmail.com>
2013-04-24 11:57:05 +0800

    Update ideasfunc1.php

--- ideasfunc1.php
+++ ideasfunc1.php
@@ -3,29 +3,6 @@
 //帮助文档:help/ideasfunc.txt
 
 
-//该函数用于获取最近更改(recentchanges)的条目(查询数量,查询种类,名称空间,筛选器,额外)
-//API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+recentchanges
-//$rclimit:指明了返回最近更改的数量,默认为5000
-//$rctype:指明了最近更改类型的过滤器,默认为新条目
-//$rcnamespace:指明了最近更改名称空间的过滤器,默认为主名称空间
-//$rcshow:指明了最近更改属性的过滤器,默认为不显示机器人更改和重定向页
-//$extra:指明了附加到POST请求的额外内容,以&开头
-function ideas_get_recent_changes($rclimit="5000",$rctype="new",$rcnamespace="0",$rcshow="!bot|!redirect",$extra=""){
-    if ($extra=""){
-        $post="action=query&list=recentchanges&rctype=".$rctype."&rclimit=".$rclimit."&rcnamespace=".$rcnamespace."&rcshow=".$rcshow;
-    }else{
-        $post="action=query&list=recentchanges&rctype=".$rctype."&rclimit=".$rclimit."&rcnamespace=".$rcnamespace."&rcshow=".$rcshow.$extra;
-    }
-    $data=ideas_connect($post);
-    //分析数据
-    $xml = simplexml_load_string($data);
-    return $xml;
-    //条目名称为:query->recentchanges->rc[$i]->attributes()->title
-    //条目编者为:query->recentchanges->rc[$i]->attributes()->user
-    //条目名称空间为:query->recentchanges->rc[$i]->attributes()->ns
-    //其他的属性:pageid,revid,timestamp......
-}
-
 //该函数用于基于条件查询条目(非重定向)列表(条件)
 //提示:该函数获得数据可能小于给定值,强烈建议预先使用count()计数以免发生错误
 //API帮助:https://zh.wikipedia.org/w/api.php?action=help&modules=query+allpages
