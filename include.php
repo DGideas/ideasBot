b27e51c41137f9fe64f106835e893f9f97d040b4
DGideas <dgideaswikipedia@gmail.com>
2013-04-29 08:19:08 +0800

    Update include.php

--- include.php
+++ include.php
@@ -3,18 +3,18 @@
     include_once ("translation.php");
     include_once ("ideasfunc1.php");
     include_once ("ideasfunc2.php");
-    include_once ("API/allpages.php");
-    include_once ("API/connect.php");
-    include_once ("API/delete.php");
-    include_once ("API/edit.php");
-    include_once ("API/imageusage.php");
-    include_once ("API/info.php");
-    include_once ("API/login.php");
-    include_once ("API/move.php");
-    include_once ("API/recentchanges.php");
-    include_once ("API/revisions.php");
-    include_once ("API/usercontribs.php");
-    include_once ("API/search.php");
+    include_once ("/API/allpages.php");
+    include_once ("/API/connect.php");
+    include_once ("/API/delete.php");
+    include_once ("/API/edit.php");
+    include_once ("/API/imageusage.php");
+    include_once ("/API/info.php");
+    include_once ("/API/login.php");
+    include_once ("/API/move.php");
+    include_once ("/API/recentchanges.php");
+    include_once ("/API/revisions.php");
+    include_once ("/API/usercontribs.php");
+    include_once ("/API/search.php");
     //include_once ("labs/cluster.php") //取消注释以启用测试功能
     //将您使用的拓展置于此处
     //include_once ("extension/checkad.php"); //取消注释以启用该拓展
